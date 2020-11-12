e4432656c0b810cdfc8d1ea1d566d9c0ddb2ad7e
Hedi <hedi@datagram.ai>
2020-11-12 10:55:59 +0100

    second

--- nbdev_bot/_nbdev.py
+++ nbdev_bot/_nbdev.py
@@ -2,9 +2,10 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"say_hello": "00_core.ipynb"}
+index = {"say_hello": "01_market-maker.ipynb"}
 
-modules = ["core.py"]
+modules = ["interface/deribit_interface.py",
+           "market-maker.py"]
 
 doc_url = "https://hedilakhdhar.github.io/nbdev_bot/"
 
